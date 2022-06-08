0fd85ce02c77e947d52ed6a99b4c5fcdfa2caf74
kleybenj <kleybenj@hu-berlin.de>
2022-06-08 13:35:28 +0200

    Revert "Implement most of A3"
    This reverts commit 0ccab1fc594bb01dd8330e9244795c7c3ec934b7.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-pub use lexer::C1Lexer;
-pub use lexer::C1Token;
-
 mod lexer;
-mod parser;
 
 // Type definition for the Result that is being used by the parser. You may change it to anything
 // you want
 pub type ParseResult = Result<(), String>;
+
+pub use lexer::C1Lexer;
+pub use lexer::C1Token;
 
 // You will need a re-export of your C1Parser definition. Here is an example:
 // mod parser;
